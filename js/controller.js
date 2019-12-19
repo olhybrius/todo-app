d54704b275bebebcf1721c08f92dc952f1f8cf46
olivier <olivier.thiel@protonmail.com>
2019-12-19 12:13:11 +0000

    fix(app): fix typo in controller method

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,8 @@
 (function (window) {
 	'use strict';
 
+
+
 	/**
 	 * Takes a model and view and acts as the controller between them
 	 *
@@ -92,7 +94,7 @@
 	 * An event to fire whenever you want to add an item. Simply pass in the event
 	 * object and it'll handle the DOM insertion and saving of the new item.
 	 */
-	Controller.prototype.adddItem = function (title) {
+	Controller.prototype.addItem = function (title) {
 		var self = this;
 
 		if (title.trim() === '') {
